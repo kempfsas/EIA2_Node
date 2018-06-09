9799b227287f23a25c6615500103a1148582b660
kempfsas <37650925+kempfsas@users.noreply.github.com>
2018-06-09 22:21:41 +0200

    server

--- Server.js
+++ Server.js
@@ -1,26 +1,27 @@
 "use strict";
-const Database = require("./Database");
 const Http = require("http");
 const Url = require("url");
+const Database = require("./Database");
 let server;
 let port = process.env.PORT;
 startServer();
 function startServer() {
     if (port == undefined)
         port = 8100;
-    server = Http.createServer();
+    let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
-    console.log('listening on port ' + port);
     server.listen(port);
 }
 function handleListen() {
-    console.log("Ich hoere...");
+    console.log('listening on port ' + port);
 }
 function handleRequest(_request, _response) {
+    console.log("Request received");
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
     let query = Url.parse(_request.url, true).query;
+    var command = query["command"];
     console.log(query["command"]);
     if (query["command"]) {
         switch (query["command"]) {
@@ -50,4 +51,61 @@ function handleRequest(_request, _response) {
         }
     }
 }
+function respond(_response, _text) {
+    //console.log("Preparing response: " + _text);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write(_text);
+    _response.end();
+}
+//if (query["command"]) {
+/*switch (query["command"]) {
+    case "insert":
+        Database.insert(JSON.parse(query["data"]));
+        _response.end();
+        break;
+
+    case "findAll":
+        Database.findAll(function(studis: string, worked: boolean) {
+            if (worked) {
+                _response.write(studis);
+                _response.end();
+            }
+
+            else
+                console.log(studis);
+        });
+        break;
+
+    case "find":
+        Database.findStudent(function (studi: string, worked: boolean) {
+            if (worked){
+                _response.write(studi);
+                _response.end();
+            }
+            else
+                console.log(studi);
+        }, parseInt(query["data"]))
+}
+}
+
+}
+
+
+export interface Studi {
+name: string;
+firstname: string;
+studiengang: string;
+matrikel: number;
+age: number;
+gender: boolean;
+}
+
+export interface Studis {
+[matrikel: string]: Studi;
+}
+
+export interface Object {
+[key: string]: string;
+}*/ 
 //# sourceMappingURL=Server.js.map
--- Server.ts
+++ Server.ts
@@ -1,6 +1,8 @@
-import * as Database from "./Database";
+
 import * as Http from "http";
 import * as Url from "url";
+import * as Database from "./Database";
+
 
 
 let server: Http.Server;
@@ -13,29 +15,71 @@ function startServer() {
     if (port == undefined)
         port = 8100;
 
-    server = Http.createServer();
+    let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
 
-    console.log('listening on port ' + port);
+    
     server.listen(port);
 }
 
 function handleListen() {
-    console.log("Ich hoere...");
+    console.log('listening on port ' + port);
 }
 
 
-
 function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse) {
+    console.log("Request received");
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
 
-    let query: Object = Url.parse(_request.url, true).query;
+    let query: AssocStringString = Url.parse(_request.url, true).query;
+    var command: string = query["command"];
     console.log(query["command"]);
 
     if (query["command"]) {
         switch (query["command"]) {
+            case "insert":
+                Database.insert(JSON.parse(query["data"]));
+                _response.end();
+                break;
+
+            case "findAll":
+                Database.findAll(function(studis: string, worked: boolean) {
+                    if (worked) {
+                        _response.write(studis);
+                        _response.end();
+                    }
+
+                    else
+                        console.log(studis);
+                });
+                break;
+
+            case "find":
+                Database.findStudent(function (studi: string, worked: boolean) {
+                    if (worked){
+                        _response.write(studi);
+                        _response.end();
+                    }
+                    else
+                        console.log(studi);
+                }, parseInt(query["data"]))
+        }
+    }
+
+}
+
+
+function respond(_response: Http.ServerResponse, _text: string): void {
+    //console.log("Preparing response: " + _text);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write(_text);
+    _response.end();
+}
+    //if (query["command"]) {
+        /*switch (query["command"]) {
             case "insert":
                 Database.insert(JSON.parse(query["data"]));
                 _response.end();
@@ -83,4 +127,4 @@ export interface Studis {
 
 export interface Object {
     [key: string]: string;
-}
+}*/
